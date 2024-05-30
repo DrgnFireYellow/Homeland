ef57dc585c7f710df888d7bb3bdd343ab80638c7
DrgnFireYellow <DrgnFireYellow@users.noreply.github.com>
2024-05-30 17:30:29 -0400

    Fix error thrown when teleporting to a player's house that is not cached

--- src/main/java/com/drgnfireyellow/homeland/commands/visit.java
+++ src/main/java/com/drgnfireyellow/homeland/commands/visit.java
@@ -14,12 +14,17 @@ public class visit implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
-            sender.sendMessage("Teleporting to "  + args[0] + "'s house...");
-            World destinationWorld = Bukkit.getWorld("homeland_" + Bukkit.getOfflinePlayerIfCached(args[0]).getUniqueId().toString());
-            Location destination = new Location(destinationWorld, 0, -60, 0);
-            ((Player) sender).teleport(destination);
-            ((Player) sender).setGameMode(GameMode.ADVENTURE);
-            ((Player) sender).getInventory().clear();
+            if (Bukkit.getOfflinePlayerIfCached(args[0]) != null) {
+                sender.sendMessage("Teleporting to "  + args[0] + "'s house...");
+                World destinationWorld = Bukkit.getWorld("homeland_" + Bukkit.getOfflinePlayerIfCached(args[0]).getUniqueId().toString());
+                Location destination = new Location(destinationWorld, 0, -60, 0);
+                ((Player) sender).teleport(destination);
+                ((Player) sender).setGameMode(GameMode.ADVENTURE);
+                ((Player) sender).getInventory().clear();
+            }
+            else {
+                sender.sendMessage("Unable to find that player's house!");
+            }
         }
         return false;
     }
