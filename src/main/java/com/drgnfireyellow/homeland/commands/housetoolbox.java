e7143f1516e4f94d9b48011ebe5b751790ee3052
DrgnFireYellow <DrgnFireYellow@users.noreply.github.com>
2024-05-30 16:00:08 -0400

    Add holograms and housetoolbox command

---
+++ src/main/java/com/drgnfireyellow/homeland/commands/housetoolbox.java
@@ -0,0 +1,56 @@
+package main.java.com.drgnfireyellow.homeland.commands;
+
+import java.util.ArrayList;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import net.kyori.adventure.text.Component;
+
+public class housetoolbox implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Inventory toolboxInventory = Bukkit.createInventory(null, 27, Component.text("House Toolbox"));
+
+            ItemStack slimeBlock = new ItemStack(Material.SLIME_BLOCK, 1);
+            ItemMeta slimeBlockMeta = slimeBlock.getItemMeta();
+            ArrayList<Component> slimeBlockLore = new ArrayList<Component>();
+            slimeBlockLore.add(Component.text("Makes you bounce really high."));
+            slimeBlockLore.add(Component.text("(Applies to all Slime Blocks)"));
+            slimeBlockMeta.displayName(Component.text("Bounce Pad"));
+            slimeBlockMeta.lore(slimeBlockLore);
+            slimeBlock.setItemMeta(slimeBlockMeta);
+            toolboxInventory.addItem(slimeBlock);
+
+            ItemStack nameTag = new ItemStack(Material.NAME_TAG, 1);
+            ItemMeta nameTagMeta = nameTag.getItemMeta();
+            ArrayList<Component> nameTagLore = new ArrayList<Component>();
+            nameTagLore.add(Component.text("Floating text. Rename in an anvil to use."));
+            nameTagLore.add(Component.text("(Applies to all Name Tags)"));
+            nameTagMeta.displayName(Component.text("Hologram"));
+            nameTagMeta.lore(nameTagLore);
+            nameTag.setItemMeta(nameTagMeta);
+            toolboxInventory.addItem(nameTag);
+
+            ItemStack hologramRemovalTool = new ItemStack(Material.BARRIER, 1);
+            ItemMeta hologramRemovalToolMeta = hologramRemovalTool.getItemMeta();
+            ArrayList<Component> hologramRemovalToolLore = new ArrayList<Component>();
+            hologramRemovalToolLore.add(Component.text("When placed, removes all nearby holograms."));
+            hologramRemovalToolMeta.lore(hologramRemovalToolLore);
+            hologramRemovalToolMeta.displayName(Component.text("Hologram Removal Tool"));
+            hologramRemovalTool.setItemMeta(hologramRemovalToolMeta);
+            toolboxInventory.addItem(hologramRemovalTool);
+
+            ((Player) sender).openInventory(toolboxInventory);
+        }
+        return false;
+    }
+}
--- src/main/java/com/drgnfireyellow/homeland/homeland.java
+++ src/main/java/com/drgnfireyellow/homeland/homeland.java
@@ -7,19 +7,28 @@ import org.bukkit.WorldType;
 import org.bukkit.WorldBorder;
 import org.bukkit.Difficulty;
 import org.bukkit.GameRule;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.util.Vector;
 import main.java.com.drgnfireyellow.homeland.commands.house;
 import main.java.com.drgnfireyellow.homeland.commands.visit;
+import net.kyori.adventure.text.Component;
 import main.java.com.drgnfireyellow.homeland.commands.housesetting;
+import main.java.com.drgnfireyellow.homeland.commands.housetoolbox;
 
 public class homeland extends JavaPlugin implements Listener {
     @Override
@@ -27,6 +36,7 @@ public class homeland extends JavaPlugin implements Listener {
         this.getCommand("house").setExecutor(new house());
         this.getCommand("visit").setExecutor(new visit());
         this.getCommand("housesetting").setExecutor(new housesetting());
+        this.getCommand("housetoolbox").setExecutor(new housetoolbox());
         Bukkit.getPluginManager().registerEvents(this, this);
     }
 
@@ -52,6 +62,41 @@ public class homeland extends JavaPlugin implements Listener {
         Block block = player.getLocation().getBlock().getRelative(BlockFace.DOWN);
         if (block.getType().equals(Material.SLIME_BLOCK) && player.getWorld().getName().startsWith("homeland_")) {
             player.setVelocity(new Vector(0f, 1.2f, 0f));
+        }
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        Material heldItemMaterial = player.getInventory().getItemInMainHand().getType();
+        if (player.getWorld().getName().startsWith("homeland_")) {
+            if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+                if (heldItemMaterial.equals(Material.NAME_TAG)) {
+                    Location blockLocation = event.getClickedBlock().getRelative(event.getBlockFace()).getLocation();
+                    ArmorStand hologramArmorStand = (ArmorStand) blockLocation.getWorld().spawnEntity(blockLocation, EntityType.ARMOR_STAND);
+                    hologramArmorStand.setGravity(false);
+                    hologramArmorStand.setSmall(true);
+                    hologramArmorStand.setCanPickupItems(false);
+                    hologramArmorStand.customName(player.getInventory().getItemInMainHand().getItemMeta().displayName());
+                    hologramArmorStand.setCustomNameVisible(true);
+                    hologramArmorStand.setVisible(false);
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        Player player = event.getPlayer();
+        if (player.getWorld().getName().startsWith("homeland_")) {
+            if (player.getInventory().getItemInMainHand().getItemMeta().displayName() != null && player.getInventory().getItemInMainHand().getItemMeta().displayName().equals(Component.text("Hologram Removal Tool"))) {
+                event.setCancelled(true);
+                for (Entity e : player.getNearbyEntities(1, 1, 1)) {
+                    if (e.getType().equals(EntityType.ARMOR_STAND)) {
+                        e.remove();
+                    }
+                }
+            }
         }
     }
 }
