7b7885be6a802421a38c30de729008986f8ca0ee
DrgnFireYellow <DrgnFireYellow@users.noreply.github.com>
2024-06-01 18:35:17 -0400

    Add error handling for housesetting

--- src/main/java/com/drgnfireyellow/homeland/commands/housesetting.java
+++ src/main/java/com/drgnfireyellow/homeland/commands/housesetting.java
@@ -16,42 +16,48 @@ public class housesetting implements CommandExecutor,TabCompleter {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             World userWorld = Bukkit.getWorld("homeland_" + ((Player) sender).getUniqueId().toString());
-            System.out.println(args[0]);
-            System.out.println(args[1]);
-            if (args[0].equals("time")) {
-                if (args[1].equals("sunrise")) {
-                    userWorld.setTime(0);
+            if (args.length > 1) {
+                if (args[0].equals("time")) {
+                    if (args[1].equals("sunrise")) {
+                        userWorld.setTime(0);
+                    }
+                    else if (args[1].equals("day")) {
+                        userWorld.setTime(1000);
+                    }
+                    else if (args[1].equals("noon")) {
+                        userWorld.setTime(6000);
+                    }
+                    else if (args[1].equals("night")) {
+                        userWorld.setTime(13000);
+                    }
+                    else if (args[1].equals("midnight")) {
+                        userWorld.setTime(18000);
+                    }
+                    else {
+                        userWorld.setTime(Integer.valueOf(args[1]));
+                    }
                 }
-                else if (args[1].equals("day")) {
-                    userWorld.setTime(1000);
-                }
-                else if (args[1].equals("noon")) {
-                    userWorld.setTime(6000);
-                }
-                else if (args[1].equals("night")) {
-                    userWorld.setTime(13000);
-                }
-                else if (args[1].equals("midnight")) {
-                    userWorld.setTime(18000);
+                else if (args[0].equals("mobspawning")) {
+                    if (args[1].equals("true")) {
+                        userWorld.setGameRule(GameRule.DO_MOB_SPAWNING, true);
+                    }
+                    else if (args[1].equals("false")) {
+                        userWorld.setGameRule(GameRule.DO_MOB_SPAWNING, false);
+                    }
+                    else {
+                        sender.sendMessage("Valid Values:");
+                        sender.sendMessage("true");
+                        sender.sendMessage("false");
+                    }
                 }
                 else {
-                    userWorld.setTime(Integer.valueOf(args[1]));
-                }
-            }
-            else if (args[0].equals("mobspawning")) {
-                if (args[1].equals("true")) {
-                    userWorld.setGameRule(GameRule.DO_MOB_SPAWNING, true);
-                }
-                else if (args[1].equals("false")) {
-                    userWorld.setGameRule(GameRule.DO_MOB_SPAWNING, false);
-                }
-                else {
-                    sender.sendMessage("Valid Options:");
-                    sender.sendMessage("true");
-                    sender.sendMessage("false");
+                    sender.sendMessage("Valid Settings:");
+                    sender.sendMessage("time");
+                    sender.sendMessage("mobspawning");
                 }
             }
             else {
+                sender.sendMessage("Please specify a setting and value.");
                 sender.sendMessage("Valid Settings:");
                 sender.sendMessage("time");
                 sender.sendMessage("mobspawning");
